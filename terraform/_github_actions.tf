68524cbc08e8cc2bb9a78eb6e8ec6514820f0b0a
Fuxing Loh <git@fuxing.dev>
2020-09-06 11:37:29 +0800

    ci: on publish, all services in ECS will be redeployed.

--- terraform/_github_actions.tf
+++ terraform/_github_actions.tf
@@ -10,3 +10,8 @@ resource "aws_iam_user_policy_attachment" "cf_web_policy" {
   policy_arn = aws_iam_policy.cf_web_policy.arn
   user       = aws_iam_user.github_actions.name
 }
+
+resource "aws_iam_user_policy_attachment" "cluster" {
+  policy_arn = aws_iam_policy.cluster.arn
+  user       = aws_iam_user.github_actions.name
+}
