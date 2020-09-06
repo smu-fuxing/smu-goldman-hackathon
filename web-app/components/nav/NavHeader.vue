69ae0528a9185e3a7c5ac7a6537e72796e3599b9
Joel-David <joel@joeldavidw.com>
2020-09-06 21:23:24 +0800

    Removed profile & strategy

--- web-app/components/nav/NavHeader.vue
+++ web-app/components/nav/NavHeader.vue
@@ -25,12 +25,6 @@
             <a href="/leaderboard" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-500 mr-6">
               Leaderboard
             </a>
-            <a href="/strategy" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-500 mr-6">
-              Strategy
-            </a>
-            <a href="/profile" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-500">
-              Profile
-            </a>
           </div>
         </div>
       </nav>
@@ -38,8 +32,6 @@
         <a href="/dashboard" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Dashboard</a>
         <a href="/academy" class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Academy</a>
         <a href="/leaderboard" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Leaderboard</a>
-        <a href="/strategy" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Strategy</a>
-        <a href="/profile" class="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800">Profile</a>
       </div>
     </div>
   </div>
