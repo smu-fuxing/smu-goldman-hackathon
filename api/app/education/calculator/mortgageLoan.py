9d33bae394590702e1c5574b5a373c3d16a1b508
dyd-x <38830767+dyd-x@users.noreply.github.com>
2020-09-07 10:39:58 +0800

    fix: mortgageLoanCalculator to take in 4 instead of 0.04

--- api/app/education/calculator/mortgageLoan.py
+++ api/app/education/calculator/mortgageLoan.py
@@ -12,7 +12,7 @@ from datetime import date
 def calculateMortgageLoan(home_price, downpayment, interest, years, payments_year, start_date):
 
     mortgage = home_price - downpayment
-
+    interest = float(interest)/100
     rng = pd.date_range(start_date, periods=years * payments_year, freq='MS')
     rng.name = "Payment Date"
     df = pd.DataFrame(index=rng, columns=['Payment', 'Principal Paid', 'Interest Paid', 'Ending Balance'], dtype='float')
