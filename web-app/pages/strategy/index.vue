0910edcc93c288c42f7567f988ca6ff54d8ca662
Joel-David <joeldavid.wong@gmail.com>
2020-09-07 17:26:32 +0800

    feat: added strategy index

--- web-app/pages/strategy/index.vue
+++ web-app/pages/strategy/index.vue
@@ -2,67 +2,38 @@
   <div class="container mx-auto pt-6 pb-48 bg-goldman-grey h-full overflow-hidden">
     <div class="px-6 full-width">
       <h1 class="text-gray-900 text-4xl">Strategy</h1>
-      <h3 class="text-gray-700 text-xl">Saving Account</h3>
-      <div class="button">Saving Account</div>
+      <h3 class="text-gray-700 text-xl">Personal Finances</h3>
 
-      <form class="mt-4" @submit.prevent="onRecommend">
-        <div v-for="input in inputs" class="py-1">
-          <div class="flex flex-col w-full">
-            <label class="block text-black text-sm font-bold mb-1">
-              {{ input.title }}
-            </label>
-
-            <input
-              :required="input.required"
-              type="number"
-              class="text-lg px-3 py-2 rounded focus:outline-none placeholder-goldman-darkBlue bg-transparent border-2 border-goldman-darkBlue text-goldman-darkBlue bg-opacity-75"
-              v-model="input.value"
-            />
-          </div>
-        </div>
-
-        <div class="mt-6 w-full">
-          <button
-                  class="w-full py-3 px-4 font-bold text-white uppercase rounded bg-goldman-darkBlue hover:bg-gray-700 focus:outline-none">
-            RECOMMEND
-          </button>
-        </div>
-      </form>
+      <div class="rounded-lg mt-4">
+        <strategy-card v-for="calculator in calculators" :key="calculator.name" class="py-4 mr-3"
+                         style="flex: 0 0 auto; width: 300px" :playlist="calculator"></strategy-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import TextInput from "~/components/academy/TextInput";
+import StrategyCard from "~/components/strategy/StrategyCard";
 
 export default {
-  components: {TextInput},
+  components: {StrategyCard},
   data() {
     return {
-      inputs: [
-        {title: 'Bank Balance (REQUIRED)', value: 0, field: 'bank_balance', required: true},
-        {title: 'Monthly Salary Credited to Bank', value: 0, field: 'monthly__salary_credited'},
-        {title: 'Monthly Credit Card Spending', value: 0, field: 'monthly_credit_card'},
-        {title: 'Total Monthly Bill Amount', value: 0, field: 'total_monthly_bill'},
-        {title: 'No. of Bills per Month', value: 0, field: 'no_bills_per_month'},
-        {title: 'Annual Lump Sum Investment', value: 0, field: 'unit_trust_annual_lum_sum_investment'},
-        {title: 'Monthly Investment', value: 0, field: 'unit_trust_monthly_investment'},
-        {title: 'Loan Monthly Installment', value: 0, field: 'home_loan_monthly_installment'},
-        {title: 'Annual Insurance Spend', value: 0, field: 'insurance_annual_expenditure'},
+      calculators: [
+        {
+          name: "Saving Accounts",
+          urlToImage: "https://dollarsandsense.sg/wp-content/uploads/2017/07/best-savings-account-2017-header.jpg",
+          type: "savings"
+        },
+        {
+          name: "Fundamental Analysis",
+          urlToImage: "https://d28wu8o6itv89t.cloudfront.net/images/FundamentalAnalysisjpg-1581061384337.jpeg",
+          type: "analysis"
+        },
       ]
     }
   },
   methods: {
-    onRecommend() {
-      const query = {}
-      this.inputs.forEach(({value, field}) => {
-          query[field] = value
-      })
-
-      this.$router.push({
-        path: '/strategy/accounts', query
-      })
-    }
   }
 }
 </script>
